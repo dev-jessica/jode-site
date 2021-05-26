33773bf8915497f62b076f2f0f36d8c78670f912
Vinicius <viiniciusjv@gmail.com>
2021-05-25 23:49:51 -0300

    Primeiro Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { RsvpComponent } from './rsvp/rsvp.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes}  from '@angular/router'
+
+const routes: Routes = [
+{path: 'inicio', component: InicioComponent},
+{path: 'historia', component: HistoriaComponent},
+{path: 'rsvp', component: RsvpComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [ RouterModule]
+  
+})
+export class AppRoutingModule { }
